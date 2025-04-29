d1aaa76c9972074295376c632f084178e8439dca
kirti <mahajankirti515@gmail.com>
2025-04-29 23:25:20 +0530

    WithInNatureBanner completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import HeroCarousel from '../Components/HeroCarousel/HeroCarousel'
 import AmazingApparel from '../Components/AmazingApparel/AmazingApparel'
 import FashionShowcase from '../Components/FashionShowcase/FashionShowcase'
+import BottomWearBanner from '../Components/BottomWearBanner/BottomWearBanner'
+import PersonalCare from '../Components/PersonalCare/PersonalCare'
+import GentlemenSlides from '../Components/gentlemenSlides/GentlemenSlides'
+import WithInNatureBanner from '../Components/BottomWearBanner/WithInNatureBanner'
 
 const Home = () => {
   return (
     <div>
         <HeroCarousel />
-        <AmazingApparel autoplayDelay={4000} />
+        <AmazingApparel autoplayDelay={2000} />
         <FashionShowcase />
+        <GentlemenSlides autoplayDelay={2000} />
+        <BottomWearBanner />
+        <PersonalCare />
+        <WithInNatureBanner />
     </div>
   )
 }
